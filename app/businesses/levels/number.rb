534d876b843a4addf5b8bcf8228d640a50806d02
Marco Castro <marcodotcastro@gmail.com>
2023-02-21 17:37:47 -0300

    feat: calculate level

--- app/businesses/levels/number.rb
+++ app/businesses/levels/number.rb
@@ -1,14 +1,31 @@
 module Levels
   class Number < BusinessApplication
+    NOTE_LEVEL_UP = 7
 
     def initialize(**params)
       @params = params
     end
 
     def call
-      level_number = Student.challenges[@params[:resource].challenge]
+      level_number = count_level_challenges
+
+      level_number = count_level_missions(level_number)
 
       @params[:resource].level_number = level_number
+    end
+
+    private
+
+    def count_level_missions(level_number)
+      level_number += 1 if @params[:resource].mission_future.present? and @params[:resource].mission_future_point >= NOTE_LEVEL_UP
+      level_number += 1 if @params[:resource].mission_base.present? and @params[:resource].mission_base_point >= NOTE_LEVEL_UP
+      level_number += 1 if @params[:resource].mission_vacancy.present? and @params[:resource].mission_vacancy_point >= NOTE_LEVEL_UP
+      level_number += 1 if @params[:resource].mission_startup.present? and @params[:resource].mission_startup_point >= NOTE_LEVEL_UP
+      level_number
+    end
+
+    def count_level_challenges
+      Student.challenges[@params[:resource].challenge].to_i
     end
 
   end
