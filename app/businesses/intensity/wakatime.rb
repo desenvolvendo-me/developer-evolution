171f2695367c60d492cc560dc9aefd374002792e
Marco Castro <marcodotcastro@gmail.com>
2023-02-23 12:19:29 -0300

    feat: wakatime_intensity strong

--- app/businesses/intensity/wakatime.rb
+++ app/businesses/intensity/wakatime.rb
@@ -25,6 +25,11 @@ module Intensity
         wakatime_intensity = "medium"
       end
 
+      if @student.wakatime_time >= 180 and wakatime_link_present
+        wakatime_intensity = "strong"
+      end
+
+
       @student.wakatime_intensity = wakatime_intensity
     end
 
