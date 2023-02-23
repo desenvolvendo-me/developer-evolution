badd05667eda4c3fca8e167da135aa76e38629b6
Marco Castro <marcodotcastro@gmail.com>
2023-02-23 10:10:46 -0300

    feat: no_testimony

---
+++ app/businesses/profiles/potential.rb
@@ -0,0 +1,18 @@
+module Profiles
+  class Potential < BusinessApplication
+    NOTE_LEVEL_UP = 7
+
+    def initialize(**params)
+      @params = params
+    end
+
+    def call
+      if @params[:resource].type_career.eql?("technology") and @params[:resource].salary >= 5000
+        level_potencial = "no_testimony"
+      end
+
+      @params[:resource].level_potencial = level_potencial
+    end
+
+  end
+end
