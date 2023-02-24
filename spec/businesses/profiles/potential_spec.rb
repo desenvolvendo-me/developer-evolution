dc4f85a2fcdbb6d1cd63edda3241a88dfca2f0e0
Marco Castro <marcodotcastro@gmail.com>
2023-02-24 15:52:52 -0300

    test: potential medium wakatime

--- spec/businesses/profiles/potential_spec.rb
+++ spec/businesses/profiles/potential_spec.rb
@@ -52,5 +52,12 @@ RSpec.describe Profiles::Potential do
       expect(@student.level_potencial).to eq("low")
     end
 
+    it 'medium' do
+      @student.update(type_career: "not_technology", salary: 2000)
+      @student.update(wakatime_time: 60)
+
+      expect(@student.level_potencial).to eq("medium")
+    end
+
   end
 end
