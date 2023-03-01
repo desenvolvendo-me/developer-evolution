25c1da72cf9472500d86ec2772e3a84cefc68f15
Marco Castro <marcodotcastro@gmail.com>
2023-03-01 18:07:48 -0300

    feat: potential high; wakatime strong

--- app/businesses/profiles/potentials/high.rb
+++ app/businesses/profiles/potentials/high.rb
@@ -10,9 +10,16 @@ module Profiles
         return unless @params[:level_potential].eql?("undefined")
         return unless @student.type_career.eql?("not_technology")
 
-        wakatime_intensity_status = ["strong", "very_strong"]
+        wakatime_power = ["strong", "very_strong"]
 
-        return unless wakatime_intensity_status.include? @student.wakatime_intensity
+        wakatime_intensity_status = ["very_weak", "weak"]
+        github_intensity_status = ["very_weak", "weak"]
+        linkedin_intensity_status = ["very_weak", "weak", "medium"]
+
+        return if (wakatime_intensity_status.include? @student.wakatime_intensity) and !wakatime_power.include? @student.wakatime_intensity
+        return if github_intensity_status.include? @student.github_intensity and !wakatime_power.include? @student.wakatime_intensity
+        return if linkedin_intensity_status.include? @student.linkedin_intensity and !wakatime_power.include? @student.wakatime_intensity
+
         level_potential = "high"
 
         @student.level_potential = level_potential
