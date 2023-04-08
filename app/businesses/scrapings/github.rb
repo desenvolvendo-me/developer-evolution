8592f6c905f7538da31b5583dfb61032e1dc386d
Marco Castro <marcodotcastro@gmail.com>
2023-04-08 16:56:34 -0300

    refactor: github total_contribution

--- app/businesses/scrapings/github.rb
+++ app/businesses/scrapings/github.rb
@@ -1,13 +1,10 @@
 class Scrapings::Github
   def initialize(args = { resource: nil })
     @student = args[:resource]
-    year = @student.enrollment_date.year
-    url = "#{@student.github_link}?tab=overview&from=#{year}-12-01&to=#{year}-12-31"
-    response = HTTParty.get(url)
-    @document = Nokogiri::HTML(response.body)
   end
 
   def download
+    read_document
     get_contributions
   end
 
@@ -27,6 +24,15 @@ class Scrapings::Github
     end
 
     @student.update(github_commit: @student.practices.sum(:commit_total))
+  end
+
+  private
+
+  def read_document
+    year = @student.enrollment_date.year
+    url = "#{@student.github_link}?tab=overview&from=#{year}-12-01&to=#{year}-12-31"
+    response = HTTParty.get(url)
+    @document = Nokogiri::HTML(response.body)
   end
 
 end
