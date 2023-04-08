4c005b44675e176829f03f03b48e47961ee6c8ee
Marco Castro <marcodotcastro@gmail.com>
2023-04-08 16:52:53 -0300

    test: github contribution total

--- spec/businesses/scrapings/github_spec.rb
+++ spec/businesses/scrapings/github_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Scrapings::Github do
   context 'total_contribution' do
     context "marcodotcastro" do
       before do
-        @student = create(:student, enrollment_date: "16/01/2022".to_date,  github_link: "https://github.com/marcodotcastro")
+        @student = create(:student, enrollment_date: "16/01/2022".to_date, github_link: "https://github.com/marcodotcastro")
       end
 
       it '2022' do
@@ -13,12 +13,13 @@ RSpec.describe Scrapings::Github do
 
         github.download
 
+        expect(@student.practices.sum(:commit_total)).to eq(550)
         expect(@student.github_commit).to eq(550)
       end
     end
     context "digspencer" do
       before do
-        @student = create(:student, enrollment_date: "16/01/2022".to_date,  github_link: "https://github.com/digspencer")
+        @student = create(:student, enrollment_date: "16/01/2022".to_date, github_link: "https://github.com/digspencer")
       end
 
       it '2022' do
@@ -26,12 +27,13 @@ RSpec.describe Scrapings::Github do
 
         github.download
 
+        expect(@student.practices.sum(:commit_total)).to eq(1)
         expect(@student.github_commit).to eq(1)
       end
     end
     context "hemershon" do
       before do
-        @student = create(:student, enrollment_date: "16/01/2021".to_date,  github_link: "https://github.com/hemershon")
+        @student = create(:student, enrollment_date: "16/01/2021".to_date, github_link: "https://github.com/hemershon")
       end
 
       it '2021' do
@@ -39,6 +41,7 @@ RSpec.describe Scrapings::Github do
 
         github.download
 
+        expect(@student.practices.sum(:commit_total)).to eq(2369)
         expect(@student.github_commit).to eq(2369)
       end
     end
