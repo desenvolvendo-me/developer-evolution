18d5126bb2704cc2a64b2cea2334074759b96cbe
Anderson Lima <freitaslimaanderson@gmail.com>
2023-06-07 10:39:34 -0300

    feat: Create model and migration Answer

---
+++ app/models/answer.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :bigint           not null, primary key
+#  tittle      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  question_id :bigint           not null
+#
+# Indexes
+#
+#  index_answers_on_question_id  (question_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (question_id => questions.id)
+#
+class Answer < ApplicationRecord
+  belongs_to :question
+end
