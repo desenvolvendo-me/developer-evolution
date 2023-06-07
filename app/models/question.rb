f83e437e7f68a7391e15bcf316aca0edb7c5f41d
Anderson Lima <freitaslimaanderson@gmail.com>
2023-06-07 10:42:49 -0300

    feat: Create model and migration Question

---
+++ app/models/question.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id             :bigint           not null, primary key
+#  tittle         :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  keyquestion_id :bigint           not null
+#
+# Indexes
+#
+#  index_questions_on_keyquestion_id  (keyquestion_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (keyquestion_id => keyquestions.id)
+#
+class Question < ApplicationRecord
+  belongs_to :keyquestion
+  has_one :answer
+end
