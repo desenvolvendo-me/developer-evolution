fefe90d16603708e9f5d7fc7a0d3d87c894919ac
isaacbrds <isaac.brigido@gmail.com>
2023-06-10 17:36:55 -0300

    test: Include relation between user and student

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -59,5 +59,7 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
 end
