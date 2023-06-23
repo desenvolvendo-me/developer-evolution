6b7e21302baef568f0f3196f65179cb6700c70d0
Marco Castro <marcodotcastro@gmail.com>
2023-06-23 17:04:43 -0300

    test: scraping wakatime average

--- spec/businesses/scrapings/wakatime_spec.rb
+++ spec/businesses/scrapings/wakatime_spec.rb
@@ -19,6 +19,30 @@ RSpec.describe Scrapings::Wakatime do
     end
   end
 
+  context 'average_activity' do
+    context "marcodotcastro" do
+      before do
+        @student = create(:student, enrollment_date: "01/02/2023".to_date, wakatime_link: "https://wakatime.com/@marcodotcastro")
+      end
+
+      it '04-2023' do
+        github = Scrapings::Wakatime.new({ resource: @student, final_enrollment_date: "30/04/2023".to_date })
+
+        github.pull_activities
+
+        expect(@student.wakatime_activity_average).to eq(638)
+      end
+
+      it '05-2023' do
+        github = Scrapings::Wakatime.new({ resource: @student, final_enrollment_date: "31/05/2023".to_date })
+
+        github.pull_activities
+
+        expect(@student.wakatime_activity_average).to eq(492)
+      end
+    end
+  end
+
   context "convert_activity_time" do
     it "3s" do
       expect(Scrapings::Wakatime.new.send(:convert_activity_to_second, "3s")).to eq(3)
