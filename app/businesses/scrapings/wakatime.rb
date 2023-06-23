56d1a091ef2c99d24940c8b43e6d7b1226e490a5
Marco Castro <marcodotcastro@gmail.com>
2023-06-23 17:04:32 -0300

    feat: scraping wakatime average

--- app/businesses/scrapings/wakatime.rb
+++ app/businesses/scrapings/wakatime.rb
@@ -7,6 +7,7 @@ class Scrapings::Wakatime
   def pull_activities
     read_document
     total_activities
+    average_activities
   end
 
   def total_activities
@@ -21,6 +22,16 @@ class Scrapings::Wakatime
     @student.update(wakatime_activity_total: @student.practices.sum(:activity_total))
   end
 
+  def average_activities
+    start_date_practice = @student.enrollment_date
+    final_date_practice = @final_enrollment_date.present? ? @final_enrollment_date : Date.today
+    days_total = (final_date_practice - start_date_practice).to_i + 1
+    commit_total = @student.practices.where(activity_date: start_date_practice..final_date_practice).sum(:activity_total)
+    commit_average = commit_total / days_total
+
+    @student.update(wakatime_activity_average: commit_average)
+  end
+
   private
 
   def read_document
@@ -31,7 +42,6 @@ class Scrapings::Wakatime
   end
 
   def convert_activity_to_second(activity_total)
-    puts activity_total
     parts = activity_total.split(" ")
 
     seconds = 0
