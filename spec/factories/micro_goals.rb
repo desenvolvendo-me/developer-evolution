bb1fcb5d645164a8b8230edbbf82023c71e04e61
gudiao21 <lucianodantas53@gmail.com>
2023-07-01 21:26:45 -0300

    test: add factory for MicroGoal model

---
+++ spec/factories/micro_goals.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: micro_goals
+#
+#  id         :bigint           not null, primary key
+#  end_date   :date
+#  micro_goal :string
+#  percentage :float
+#  start_date :date
+#  task       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  student_id :bigint
+#
+# Indexes
+#
+#  index_micro_goals_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
+#
+FactoryBot.define do
+  factory :micro_goal do
+    micro_goal { "MyString" }
+    task { "MyString" }
+    start_date { "2023-06-30" }
+    end_date { "2023-06-30" }
+    percentage { 1.5 }
+  end
+end
