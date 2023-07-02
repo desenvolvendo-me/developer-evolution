b3121e8d684f124e4b78214252965fede63f0546
gudiao21 <lucianodantas53@gmail.com>
2023-07-02 15:21:53 -0300

    feat: Add factory for MicroGoal Model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,7 +18,7 @@
 #
 FactoryBot.define do
   factory :user do
-    email { "student@email.com" }
-    password {"123456"}
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { 'password' }
   end
 end
