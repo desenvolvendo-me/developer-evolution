4b55da84ec16d435f20315c21eb58e5d9bd164a6
gudiao21 <lucianodantas53@gmail.com>
2023-07-01 21:14:30 -0300

    refactor: update validation in MicroGoal model to include 'student_id'

--- app/models/micro_goal.rb
+++ app/models/micro_goal.rb
@@ -10,6 +10,17 @@
 #  task       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  student_id :bigint
+#
+# Indexes
+#
+#  index_micro_goals_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
 #
 class MicroGoal < ApplicationRecord
+  belongs_to :student
+  validates :student_id, :micro_goal, :task, :start_date, :end_date, :percentage, presence: true
 end
