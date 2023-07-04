2cae1cb55b23c2ac79d1131375fa74e90547b99e
Philipe <dev.philipe@outlook.com>
2023-07-04 12:23:09 -0300

    revert(Experience): scaffold destroyed

--- db/migrate/20230704150829_add_columns_to_experiences.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToExperiences < ActiveRecord::Migration[7.0]
-  def change
-    add_column :experiences, :category, :string
-    add_column :experiences, :level, :integer
-    add_column :experiences, :week, :integer
-  end
-end
