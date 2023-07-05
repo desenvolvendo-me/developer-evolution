eae306a22ea4b98e9e10b0446a483181870783f4
Philipe <dev.philipe@outlook.com>
2023-07-05 16:23:43 -0300

    test(Experience): first test created

--- spec/models/experience_spec.rb
+++ spec/models/experience_spec.rb
@@ -1,5 +1,34 @@
+# == Schema Information
+#
+# Table name: experiences
+#
+#  id         :bigint           not null, primary key
+#  category   :string
+#  content    :text
+#  level      :integer
+#  week       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  student_id :bigint           not null
+#
+# Indexes
+#
+#  index_experiences_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
+#
 require 'rails_helper'
 
 RSpec.describe Experience, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:student) { FactoryBot.create(:student) }
+
+    it 'is valid with valid attributes' do
+      experience = FactoryBot.create(:experience, student: student)
+      expect(experience).to be_valid
+    end
+
+  end
 end
