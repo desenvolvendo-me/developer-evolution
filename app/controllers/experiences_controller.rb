4973135888f8339847f93c42146de09dfd41f963
Philipe <dev.philipe@outlook.com>
2023-07-05 12:37:52 -0300

    fix(Experience controller): controller refering to student and working now

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -4,7 +4,14 @@ class ExperiencesController < InheritedResources::Base
     @experience = Experience.new
   end
   def create
-    @experience = current_user.experiences.build(experience_params)
+    @experience = current_user.student.experiences.build(experience_params)
+
+    if @experience.save
+      flash[:success] = 'Cadastro realizado com sucesso'
+      redirect_to experiences_path
+    else
+      render 'new'
+    end
   end
 
   private
