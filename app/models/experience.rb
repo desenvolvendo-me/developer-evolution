dec496b38ee95c849443c190ec03ce61340012b4
Philipe <dev.philipe@outlook.com>
2023-07-05 17:16:39 -0300

    fix(Experience model): validation to content field created

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -21,6 +21,8 @@
 #
 class Experience < ApplicationRecord
   belongs_to :student
+
+  validates :content, presence: true
   validates :category, inclusion: { in: ['Medo', 'Dificuldade', 'Aprendizado', 'Insight', 'Feedback'] }
   validates :level, inclusion: { in: 1..8 }
   validates :week, inclusion: { in: 1..4 }, if: :level_previously_changed?
