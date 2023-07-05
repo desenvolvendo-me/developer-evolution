c346f5047a9e710a85a4066a7922f07d608a707d
Philipe <dev.philipe@outlook.com>
2023-07-05 11:09:09 -0300

    build(Experience entity): migrated do database

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: experiences
+#
+#  id         :bigint           not null, primary key
+#  category   :string
+#  content    :text
+#  level      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  student_id :bigint           not null
+#
+# Indexes
+#
+#  index_experiences_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
+#
 class Experience < ApplicationRecord
   belongs_to :student
 end
--- spec/factories/experiences.rb
+++ spec/factories/experiences.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: experiences
+#
+#  id         :bigint           not null, primary key
+#  category   :string
+#  content    :text
+#  level      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  student_id :bigint           not null
+#
+# Indexes
+#
+#  index_experiences_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
+#
 FactoryBot.define do
   factory :experience do
     student { nil }
