38988c5b2cf01296e30647936b2e32cf398f0e9d
Philipe <dev.philipe@outlook.com>
2023-07-05 15:30:45 -0300

    chore(schema): week changed place

--- db/schema.rb
+++ db/schema.rb
@@ -45,9 +45,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_153213) do
     t.text "content"
     t.string "category"
     t.integer "level"
+    t.integer "week"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "week"
     t.index ["student_id"], name: "index_experiences_on_student_id"
   end
 
