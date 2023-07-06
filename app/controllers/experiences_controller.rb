a3d37068f36c7ac8d6727fc243d9336d06462f0d
Philipe <dev.philipe@outlook.com>
2023-07-06 17:04:29 -0300

    test: Experience controller tests created

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,25 +1,35 @@
 class ExperiencesController < InheritedResources::Base
-
+  before_action :set_experience, only: [:show]
+  def index
+    @experiences = current_user.student.experiences.all
+  end
   def new
     @experience = Experience.new
-    render :new
   end
   def create
     @experience = current_user.student.experiences.build(experience_params)
 
     if @experience.save
       flash[:notice] = 'Experiência cadastrada com sucesso'
-      redirect_to experiences_path
+      redirect_to action: :show, id: @experience.id
     else
       flash[:alert] = @experience.errors.full_messages.join('. ')
       redirect_to experiences_path
     end
   end
 
+  def show
+
+  end
+
   private
 
-    def experience_params
-      params.require(:experience).permit(:student_id, :content, :category, :level, :week)
-    end
+  def experience_params
+    params.require(:experience).permit(:student_id, :content, :category, :level, :week)
+  end
+
+  def set_experience
+    @experience = current_user.student.experiences.find(params[:id])
+  end
 
 end
