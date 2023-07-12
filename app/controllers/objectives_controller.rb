2d8dd9cb19661eb794dcf4ee682741a581aaf78f
Philipe <dev.philipe@outlook.com>
2023-07-12 18:44:24 -0300

    feat(objectives controller): actions created

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,9 +1,50 @@
-class ObjectivesController < InheritedResources::Base
+class ObjectivesController < ApplicationController
+  before_action :set_objective, only: [:show, :edit, :update, :destroy]
+  def index
+    @objectives = current_user.student.objectives.all
+  end
+
+  def show
+  end
+
+  def new
+    @objective = Objective.new
+  end
+
+  def create
+    @objective = current_user.student.objectives.build(objective_params)
+
+    if @objective.save
+      redirect_to objectives_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+
+    if @objective.update(objective_params)
+      redirect_to @objective
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @objective.destroy
+
+    redirect_to objectives_path, status: :see_other
+  end
 
   private
 
-    def objective_params
-      params.require(:objective).permit(:student_id, :title, :description, :reason)
-    end
-
+  def objective_params
+    params.require(:objective).permit(:title, :description, :reason)
+  end
+  def set_objective
+    @objective = current_user.student.objectives.find(params[:id])
+  end
 end
