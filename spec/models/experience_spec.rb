55159800545ffa9a949e673211eda0aa95587a78
Marco Castro <marcodotcastro@gmail.com>
2023-07-13 17:39:21 -0300

    test: experiences

--- spec/models/experience_spec.rb
+++ spec/models/experience_spec.rb
@@ -25,21 +25,12 @@ RSpec.describe Experience, type: :model do
   describe 'validations' do
     let(:student) { FactoryBot.create(:student) }
 
-    it 'is valid with valid attributes' do
-      experience = FactoryBot.create(:experience, student: student)
-      expect(experience).to be_valid
-    end
-
-    it 'is invalid without attribute week' do
-      experience = FactoryBot.create(:experience, student: student, week: nil)
-      expect(experience).to be_invalid
-    end
-
-    it 'is invalid without attribute level' do
-      invalid_level = rand(9..100)
-      experience = FactoryBot.create(:experience, student: student)
-      experience.assign_attributes(level: invalid_level)
-      expect(experience).to be_invalid
+    describe 'associations' do
+      it { should belong_to(:student) }
+      it { should validate_presence_of(:content) }
+      it { should validate_inclusion_of(:week).in_range(1..4) }
+      it { should validate_inclusion_of(:level).in_range(1..8) }
+      it { should validate_inclusion_of(:category).in_array(Experience::CATEGORY) }
     end
 
   end
