5c54533312a5233e6f1aa235c3615eaa202aa89a
PhilipeeX <dev.philipe@outlook.com>
2023-07-14 17:09:09 -0300

    feat(objective controller): actions defined

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,9 +1,51 @@
-class ObjectivesController < InheritedResources::Base
+class ObjectivesController < ApplicationController
+  before_action :set_objective, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @objectives = current_user.student.objectives.all
+  end
+
+  def new
+    @objective = Objective.new
+  end
+
+  def create
+    @objective = current_user.student.objectives.build(objective_params)
+
+    if @objective.save
+      redirect_to @objective
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @objective.update(objective_params)
+      redirect_to @objective
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @objective.destroy
+
+    redirect_to objectives_path, status: :see_other
+  end
 
   private
 
-    def objective_params
-      params.require(:objective).permit(:title, :reason, :people_involved, :resources, :expected_results, :achieved, :metrics, :max_deadline, :min_deadline, :student_id)
-    end
+  def set_objective
+    @objective = current_user.student.objectives.find(params[:id])
+  end
 
+  def objective_params
+    params.require(:objective).permit(:title, :reason, :people_involved, :resources, :expected_results, :achieved, :metrics, :max_deadline, :min_deadline)
+  end
 end
