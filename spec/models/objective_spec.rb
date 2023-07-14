30c16abd968304fbc1357d2eb671d60261ae61c5
PhilipeeX <dev.philipe@outlook.com>
2023-07-14 17:12:48 -0300

    test(Objective model): test created using gem shoulda-matchers

--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -1,5 +1,33 @@
+# == Schema Information
+#
+# Table name: objectives
+#
+#  id               :bigint           not null, primary key
+#  achieved         :text
+#  expected_results :text
+#  max_deadline     :date
+#  metrics          :text
+#  min_deadline     :date
+#  people_involved  :text
+#  reason           :text
+#  resources        :text
+#  title            :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  student_id       :bigint           not null
+#
+# Indexes
+#
+#  index_objectives_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
+#
 require 'rails_helper'
 
 RSpec.describe Objective, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:student) }
+  end
 end
