893fb5f7da1b82bf4af0dfd32e937876e92d13c5
gudiao21 <lucianodantas53@gmail.com>
2023-07-14 17:25:18 -0300

    feat: Add default locale to i18n pt-BR in config/application.rb file

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module DeveloperEvolution
   class Application < Rails::Application
+    config.i18n.default_locale = :'pt-BR'
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
