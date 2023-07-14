24352c6060b8366652b88578838e9f2d3f85fcec
gudiao21 <lucianodantas53@gmail.com>
2023-07-14 17:31:26 -0300

    build: remove 'before_create callback' from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,13 +18,11 @@
 #
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one :student
-  before_create :build_student
-  
+
   def to_s
     email
   end
