cb9a2df2917b8258cabbb89dd60e47a22c9555f5
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-07-14 23:34:22 -0300

    feat: Removed InheritedResources::Base and added ApplicationController

--- app/controllers/purposes_controller.rb
+++ app/controllers/purposes_controller.rb
@@ -1,25 +1,48 @@
 class PurposesController < ApplicationController
 
+
+  def show
+    @purpose = Purpose.find(params[:id])
+  end
+
   def new
     @purpose = Purpose.new
   end
   def create
-    if current_user.student
-      @purpose = current_user.student.purpose.new(purpose_params)
-      if @purpose.save
-        redirect_to @purpose, notice: "Purpose was successfully created."
-      else
-        flash[:notice] = "There were some errors with your purpose."
-        render :new
-      end
+    @purpose = Purpose.new(purpose_params)
+    @purpose.student = current_user.student
+
+    if @purpose.save
+      redirect_to @purpose, notice: "Purpose was successfully created."
     else
-      flash[:notice] = "You must have a student associated with you to create a purpose."
+      flash[:notice] = "There were some errors with your purpose."
       render :new
+    end
+  end
+
+  def edit
+    @purpose = Purpose.find(params[:id])
+  end
+
+  def update
+    @purpose = Purpose.find(params[:id])
+    if @purpose.update(purpose_params)
+      flash[:notice] = 'Propósito atualizado com sucesso.'
+      redirect_to purpose_path(@purpose)
+    else
+      render 'edit'
     end
   end
 
   def index
     @purposes = current_user.student.purposes
+  end
+
+  def destroy
+    @purpose = Purpose.find(params[:id])
+    @purpose.destroy
+    flash[:notice] = 'Propósito excluído com sucesso.'
+    redirect_to purposes_path
   end
 
 
