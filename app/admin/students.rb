d2ba8f46052cb76369e44058b0bdb3bc1d2b3da5
Marco Castro <marcodotcastro@gmail.com>
2023-07-17 12:21:17 -0300

    fix: add slug in admin

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -105,4 +105,14 @@ ActiveAdmin.register Student do
       row :mission_startup_point
     end
   end
+
+  controller do
+    def find_resource
+      begin
+        scoped_collection.where(slug: params[:id]).first!
+      rescue ActiveRecord::RecordNotFound
+        scoped_collection.find(params[:id])
+      end
+    end
+  end
 end
