3501702aacb6a123b49b18c8e8c446cda8429c58
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-07-18 13:23:05 -0300

    feat: Using Shoulda matchers

--- spec/models/purpose_spec.rb
+++ spec/models/purpose_spec.rb
@@ -23,55 +23,7 @@
 require 'rails_helper'
 
 RSpec.describe Purpose, type: :model do
-
-  context 'Validar type question de purpose' do
-    let(:student) { FactoryBot.create(:student) }
-
-    it 'The Type Question field is valid' do
-      purpose = FactoryBot.create(:purpose, student: student)
-      expect(purpose).to be_valid
-    end
-
-    it 'The Type Question field is valid ' do
-      PURPOSE = %w[Why How When Where]
-      purpose = FactoryBot.create(:purpose, student: student, type_question: PURPOSE.sample)
-      expect(purpose).to be_valid
-    end
-
-    # it 'The Type Question field is invalid ' do
-    #   PURPOSE = FFaker::Lorem.word
-    #   purpose = FactoryBot.create(:purpose, student: student, type_question: PURPOSE)
-    #   expect(purpose).to_not be_valid
-    # end
-  end
-
-  context 'Validar Question de purpose' do
-    let(:student) { FactoryBot.create(:student) }
-
-    it 'The Question field is valid' do
-      purpose = FactoryBot.create(:purpose, student: student)
-      expect(purpose).to be_valid
-    end
-
-    it 'The Question field is valid ' do
-      PURPOSE = %w[because its good when Im on vacation]
-      purpose = FactoryBot.create(:purpose, student: student, question: PURPOSE.sample)
-      expect(purpose).to be_valid
-    end
-  end
-
-  context 'Validar Answer de purpose' do
-    let(:student) { FactoryBot.create(:student) }
-
-    it 'The Answer field is valid' do
-      purpose = FactoryBot.create(:purpose, student: student)
-      expect(purpose).to be_valid
-    end
-
-    it 'The Answer field is valid ' do
-      PURPOSE = %w[programming is good programming is life]
-      purpose = FactoryBot.create(:purpose, student: student, answer: PURPOSE.sample)
-      expect(purpose).to be_valid
-    end
+  describe 'associations' do
+    it { should belong_to(:student) }
   end
 end
