a00cd3d6081aed1c9ac2b45ffc85f478fb77e965
Marco Castro <marcodotcastro@gmail.com>
2023-07-20 16:54:14 -0300

    refactor: routine priority

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -21,14 +21,10 @@
 #
 class Routine < ApplicationRecord
   belongs_to :student
-  enum day_of_the_week: [ "domingo",  "segunda", "terça", "quarta", "quinta", "sexta", "sábado" ]
+
+  PRIORITY = ['1', '2', '3', '4', '5']
+
+  enum day_of_the_week: ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"]
   validates :activity, :priority, presence: true
 
-  #Estou com dúvida se é realmente necessário essas linhas abaixo:
-  def self.weekly_activities
-    [
-      { day_of_th_week: 1, time: '08:00', activity: 'Study', priority: 'high'},
-      { day_of_the_week: 2, time: '10:00', activity: 'work', priority: 'medium'}
-    ]
-  end
 end
