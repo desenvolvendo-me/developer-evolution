14823929220eca1dca7162589d02084092e8e2bb
gudiao21 <lucianodantas53@gmail.com>
2023-07-23 20:27:01 -0300

    build: Add comment to app/models/task_spec.rb file

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,13 +2,22 @@
 #
 # Table name: tasks
 #
-#  id         :bigint           not null, primary key
-#  end_date   :date
-#  percentage :float
-#  start_date :date
-#  task       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :bigint           not null, primary key
+#  end_date      :date
+#  percentage    :float
+#  start_date    :date
+#  task          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  micro_goal_id :bigint           not null
+#
+# Indexes
+#
+#  index_tasks_on_micro_goal_id  (micro_goal_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (micro_goal_id => micro_goals.id)
 #
 class Task < ApplicationRecord
   belongs_to :micro_goal
