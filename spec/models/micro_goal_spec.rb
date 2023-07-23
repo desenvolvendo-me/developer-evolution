10fa172aa5e3f7efe5abaed69be38df405e0c5de
gudiao21 <lucianodantas53@gmail.com>
2023-07-23 17:05:11 -0300

    chore: Add important comments for micro_goal_spec.rb

--- spec/models/micro_goal_spec.rb
+++ spec/models/micro_goal_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: micro_goals
+#
+#  id         :bigint           not null, primary key
+#  micro_goal :string
+#  string     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe MicroGoal, type: :model do
