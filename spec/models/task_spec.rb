1a32cc0e5ea6e8aadf791b0ed20703dbe55e6388
gudiao21 <lucianodantas53@gmail.com>
2023-07-23 17:05:31 -0300

    chore: Add important comments for task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  end_date   :date
+#  percentage :float
+#  start_date :date
+#  task       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
