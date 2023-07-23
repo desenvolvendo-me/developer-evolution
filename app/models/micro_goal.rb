a9e9cefd9f20f13d27e380fc7025dd3fd6f34976
gudiao21 <lucianodantas53@gmail.com>
2023-07-23 17:04:40 -0300

    chore: Add important comments

--- app/models/micro_goal.rb
+++ app/models/micro_goal.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: micro_goals
+#
+#  id         :bigint           not null, primary key
+#  micro_goal :string
+#  string     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class MicroGoal < ApplicationRecord
   belongs_to :student
   has_many :tasks
