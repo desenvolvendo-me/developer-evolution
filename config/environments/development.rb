1cf52ef1372688617545bad1e7d4ac033cbe49ed
gudiao21 <lucianodantas53@gmail.com>
2023-07-23 15:41:28 -0300

    style: add 'i18n' to 'config/environments/development.rb'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # Configuração do I18n:
+  config.i18n.default_locale = :'pt-BR'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
