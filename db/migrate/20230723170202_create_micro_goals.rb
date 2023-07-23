ac16c0c27702e270744dccbdf285ef2bedce7f83
gudiao21 <lucianodantas53@gmail.com>
2023-07-23 16:13:14 -0300

    feat: create micro_goal migration

---
+++ db/migrate/20230723170202_create_micro_goals.rb
@@ -0,0 +1,10 @@
+class CreateMicroGoals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :micro_goals do |t|
+      t.string :micro_goal
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
