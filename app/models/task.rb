2894adaebd1366fbd8826530a66d80484ed1079b
gudiao21 <lucianodantas53@gmail.com>
2023-07-23 17:01:21 -0300

    fix: fix ':' syntaxe erro on second line on task.rb model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  end_date   :date
+#  percentage :float
+#  start_date :date
+#  task       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Task < ApplicationRecord
-  belongs_to: micro_goal
+  belongs_to :micro_goal
 end
