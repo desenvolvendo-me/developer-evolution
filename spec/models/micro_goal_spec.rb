7b88f20fc526db1ae07bdf49c9431a622e3979f6
gudiao21 <lucianodantas53@gmail.com>
2023-07-23 15:09:28 -0300

    test: create micro_goal_spec.rb rspec test.

--- spec/models/micro_goal_spec.rb
+++ spec/models/micro_goal_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe MicroGoal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:student) { FactoryBot.create(:student) }
+
+    it "is valid with valid attributes" do
+      micro_goal = FactoryBot.create(:micro_goal, student: student)
+      expect(micro_goal).to be_valid
+    end
+
+    it "is invalid without a micro_goal attribute" do
+      micro_goal = MicroGoal.new(micro_goal: nil)
+      expect(micro_goal).not_to be_valid
+    end
+
+    it "belongs to a student" do
+      user = User.create!(email:"alice@example.com", password: "password")
+      student = Student.create(name: "Alice", user: user)
+      student.save!
+      micro_goal = student.micro_goals.create!(micro_goal: "logic programming")
+      expect(micro_goal.student).to eq(student)
+    end
+
+    it "has many tasks" do
+      micro_goal = FactoryBot.create(:micro_goal, student: student)
+      task1 = FactoryBot.create(:task, micro_goal: micro_goal)
+      task2 = FactoryBot.create(:task, micro_goal: micro_goal)
+
+      expect(micro_goal.tasks).to include(task1, task2)
+    end
+  end
 end
