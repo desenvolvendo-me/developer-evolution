462143cb12d6e06b61849e248868d7c72963b097
PhilipeeX <dev.philipe@outlook.com>
2023-07-24 19:45:15 -0300

    feat(objective_question): relation between objective and obj question

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: objectives
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Objective < ApplicationRecord
+  has_many :objective_questions
 end
