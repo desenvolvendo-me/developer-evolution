afb11369d08e2b9c60af534a0d44f8486e4eb588
PhilipeeX <dev.philipe@outlook.com>
2023-07-24 19:48:28 -0300

    test(objective and obj question): model test created

--- spec/factories/objectives.rb
+++ spec/factories/objectives.rb
@@ -1,5 +1,14 @@
+# == Schema Information
+#
+# Table name: objectives
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :objective do
-    title { "MyString" }
+    title { "OBJETIVO 1: Tornar-me um pogramador PHP/Laravel pleno" }
   end
 end
--- spec/models/objective_question_spec.rb
+++ spec/models/objective_question_spec.rb
@@ -1,5 +1,27 @@
+# == Schema Information
+#
+# Table name: objective_questions
+#
+#  id            :bigint           not null, primary key
+#  answer        :string
+#  description   :string
+#  type_question :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  objective_id  :bigint           not null
+#
+# Indexes
+#
+#  index_objective_questions_on_objective_id  (objective_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (objective_id => objectives.id)
+#
 require 'rails_helper'
 
 RSpec.describe ObjectiveQuestion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do
+    it { should belong_to(:objective) }
+  end
 end
