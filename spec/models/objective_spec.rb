d5267d63f2e05f248a90dd3a4823f0bae940e82d
PhilipeeX <dev.philipe@outlook.com>
2023-07-24 19:50:28 -0300

    test(objective): model test created

--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -1,5 +1,16 @@
+# == Schema Information
+#
+# Table name: objectives
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Objective, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do
+    it { should have_many(:objective_questions) }
+  end
 end
