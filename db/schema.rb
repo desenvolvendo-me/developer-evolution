4f72a8921bb88dd3e425d0b8a6dd5c053f60a35f
Philipe <dev.philipe@outlook.com>
2023-07-25 16:50:45 -0300

    build(schema): insterview database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_08_193316) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_25_190629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_08_193316) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "interviews", force: :cascade do |t|
+    t.string "interview_type"
+    t.integer "video_number"
+    t.string "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "practices", force: :cascade do |t|
