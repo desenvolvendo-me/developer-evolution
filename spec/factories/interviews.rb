fdbb3336985e688a63dbe9313b525c5bd549ad51
Philipe <dev.philipe@outlook.com>
2023-07-25 16:57:28 -0300

    test(interview):

--- spec/factories/interviews.rb
+++ spec/factories/interviews.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: interviews
+#
+#  id             :bigint           not null, primary key
+#  answer         :string
+#  interview_type :string
+#  video_number   :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  student_id     :bigint           not null
+#
+# Indexes
+#
+#  index_interviews_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
+#
 FactoryBot.define do
   factory :interview do
     interview_type { "Disciplina" }
--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -1,5 +1,27 @@
+# == Schema Information
+#
+# Table name: interviews
+#
+#  id             :bigint           not null, primary key
+#  answer         :string
+#  interview_type :string
+#  video_number   :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  student_id     :bigint           not null
+#
+# Indexes
+#
+#  index_interviews_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
+#
 require 'rails_helper'
 
 RSpec.describe Interview, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:student) }
+  end
 end
