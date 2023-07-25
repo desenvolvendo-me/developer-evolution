d2f6f2ca880bbf6ab54bd0bf39bfce2ab34a5997
PhilipeeX <dev.philipe@outlook.com>
2023-07-25 08:47:57 -0300

    fix(objective): relation between obj and student

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -6,7 +6,17 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  student_id :bigint           not null
+#
+# Indexes
+#
+#  index_objectives_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
 #
 class Objective < ApplicationRecord
+  belongs_to :student
   has_many :objective_questions
 end
