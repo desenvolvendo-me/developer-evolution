08d55daacb005ab27e2005b6f424c6c2e57188b5
Philipe <dev.philipe@outlook.com>
2023-07-25 16:50:17 -0300

    feat(interview model): constants created

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: interviews
+#
+#  id             :bigint           not null, primary key
+#  answer         :string
+#  interview_type :string
+#  video_number   :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class Interview < ApplicationRecord
+  INTERVIEW_TYPE = ["VIDA", "RESILIÊNCIA", "SUPERAÇÃO", "FOCO E DISCIPLINA", "EMPRESA", "PORTFÓLIO"]
+  VIDEO_NUMBER = [1,2,3,4,5]
 end
