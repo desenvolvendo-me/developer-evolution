372a5bc2cced8d4e86f1298b3e9e10af05b2a191
Gustavo Ramos <gu.gugovisk1@gmail.com>
2023-07-25 22:51:55 -0300

    fix: another some fix for technical tests

--- app/controllers/technical_tests_controller.rb
+++ app/controllers/technical_tests_controller.rb
@@ -1,2 +1,51 @@
 class TechnicalTestsController < ApplicationController
+  before_action :set_technical_test, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @technical_tests = current_user.student.technical_tests.all
+  end
+
+  def show
+  end
+
+  def new
+    @technical_test = TechnicalTest.new
+  end
+
+  def create
+    @technical_test = current_user.student.technical_tests.build(technical_test_params)
+
+    if @technical_test.save
+      redirect_to @technical_test, notice: 'Technical Test was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @technical_test.update(technical_test_params)
+      redirect_to @technical_test, notice: 'Technical Test was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @technical_test.destroy
+    redirect_to technical_tests_url, notice: 'Technical Test was successfully destroyed.'
+  end
+
+  private
+
+  def set_technical_test
+    @technical_test = TechnicalTest.find(params[:id])
+  end
+
+  def technical_test_params
+    params.require(:technical_test).permit(:name, :project, :readme, :link, :battle, :release_type)
+  end
 end
+
--- app/models/technical_test.rb
+++ app/models/technical_test.rb
@@ -23,4 +23,8 @@
 #
 class TechnicalTest < ApplicationRecord
   belongs_to :student
+
+  CATEGORY = ['Milestone Release', 'Pull Request Release', 'Pull Request Branch 1', 'Pull Request Branch 2', 'Pull Request Branch 3', 'Pull Request Branch 4', 'Pull Request Branch 5', 'Pull Request Branch 6', 'Pull Request Branch 7', 'Pull Request Branch 8', 'Issue 1', 'Issue 2', 'Issue 3', 'Issue 4', 'Issue 5', 'Issue 6', 'Issue 7', 'Issue 8']
+
+
 end
