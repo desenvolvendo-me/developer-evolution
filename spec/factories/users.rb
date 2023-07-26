e59387ff12f1335b5d313f2d099ed06ac1895331
Marco Castro <marcodotcastro@gmail.com>
2023-07-26 11:56:57 -0300

    fix: user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,7 +18,7 @@
 #
 FactoryBot.define do
   factory :user do
-    email { "student@email.com" }
-    password {"123456"}
+    sequence(:email) { |n| "student#{n}@email.com" }
+    password { "123456" }
   end
 end
