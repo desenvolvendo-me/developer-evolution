0d822272e650e9cbbabea75587ca2fa2980073ad
Philipe <dev.philipe@outlook.com>
2023-07-27 14:56:26 -0300

    feat(student and objectives): Relation between them

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,22 +1,55 @@
 class ObjectivesController < InheritedResources::Base
-
+  before_action :set_objective, only: %i[ show edit update destroy ]
+  def index
+    @objectives = current_user.student.objectives.all
+  end
+  def show
+  end
+  def new
+    @objective = current_user.student.objectives.new
+  end
 
   def show
-    @objective = Objective.find(params[:id])
+    @objective = current_user.student.objectives.find(params[:id])
+  end
+
+  def create
+    @objective = current_user.student.objectives.new(objective_params)
+
+    if @objective.save
+      redirect_to objective_url(@objective), notice: I18n.t('objective.controller.notice_create')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+  def update
+
+    if @objective.update(objective_params)
+      redirect_to objective_url(@objective), notice: I18n.t('objective.controller.notice_update')
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @objective = Objective.find(params[:id])
+    @objective = current_user.student.objectives.find(params[:id])
     @objective.objective_questions.destroy_all
     @objective.destroy
 
-    redirect_to objectives_path, notice: 'Objective and associated questions were successfully destroyed.'
+    redirect_to objectives_path, notice: I18n.t('objective.controller.destroy')
   end
 
   private
 
-    def objective_params
-      params.require(:objective).permit(:title)
-    end
+  def set_objective
+    @objective = current_user.student.objectives.find(params[:id])
+  end
+
+  def objective_params
+    params.require(:objective).permit(:title)
+  end
 
 end
