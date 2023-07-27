26c1ed42a1439c19a22082d5f8a91ec517fff757
Philipe <dev.philipe@outlook.com>
2023-07-27 15:45:30 -0300

    feat(interview_controller): relation created between student and interview

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,9 +1,46 @@
-class InterviewsController < InheritedResources::Base
+class InterviewsController < ApplicationController
+  before_action :set_interview, only: [:show, :update, :edit, :destroy]
+  def index
+    @interviews = current_user.student.interviews
+  end
+  def new
+    @interview = Interview.new
+  end
+  def create
+    @interview = current_user.student.interviews.build(interview_params)
 
+    if @interview.save
+      flash[:notice] = I18n.t('interview.controller.flash_create')
+      redirect_to action: :show, id: @interview.id
+    else
+      flash[:alert] = @interview.errors.full_messages.join('. ')
+      redirect_to interviews_path
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @interview.update(interview_params)
+      flash[:notice] = I18n.t('interview.controller.flash_update')
+      redirect_to action: :show, id: @interview.id
+    else
+      redirect_to action: :edit
+    end
+  end
   private
 
-    def interview_params
-      params.require(:interview).permit(:interview_type, :video_number, :answer)
-    end
+  def interview_params
+    params.require(:interview).permit(:interview_type, :video_number, :answer)
+  end
+
+  def set_interview
+    @interview = current_user.student.interviews.find(params[:id])
+  end
 
 end
