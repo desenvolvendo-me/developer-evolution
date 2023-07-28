b0cc5047c3674e46f62d17e1e603885e789f3525
Gustavo Ramos <gu.gugovisk1@gmail.com>
2023-07-28 13:27:49 -0300

    config: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :scripts do
+    resources :script_questions
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
