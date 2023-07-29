6be000925dbcf50356e93cc7d82eb9d68561e0e4
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-07-29 11:40:56 -0300

    feat(routes): Apply nested resources method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :purposes
+  resources :purposes do
+    resources :purpose_questions, module: :purposes
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
