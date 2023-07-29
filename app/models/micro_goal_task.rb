2a070df35ec8a08702572d70c185dac997103b89
gudiao21 <lucianodantas53@gmail.com>
2023-07-29 18:54:44 -0300

    feat(model): create ralation between MicroGoalTask to MicroGoal table

---
+++ app/models/micro_goal_task.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: micro_goal_tasks
+#
+#  id            :bigint           not null, primary key
+#  end_date      :date
+#  percentage    :float
+#  start_date    :date
+#  task          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  micro_goal_id :bigint           not null
+#
+# Indexes
+#
+#  index_micro_goal_tasks_on_micro_goal_id  (micro_goal_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (micro_goal_id => micro_goals.id)
+#
+class MicroGoalTask < ApplicationRecord
+  belongs_to :micro_goal
+end
