03ece7de93427ba91162d25871caccf16b3ece17
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-07-29 11:33:12 -0300

    feat(migrate): Remove purpose attribute type_question description time answer

--- db/migrate/20230707194844_create_purposes.rb
+++ db/migrate/20230707194844_create_purposes.rb
@@ -2,10 +2,7 @@ class CreatePurposes < ActiveRecord::Migration[7.0]
   def change
     create_table :purposes do |t|
       t.integer :version
-      t.integer :time
-      t.string :type_question
-      t.text :question
-      t.text :answer
+      t.references :student, null: false, foreign_key: true
 
       t.timestamps
     end
