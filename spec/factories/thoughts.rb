e6c4548518bf8d10697886e0f1bebd4d84649a35
gudiao21 <lucianodantas53@gmail.com>
2023-08-01 19:04:02 -0300

    feat(factory): create a record by 'FactoryBot' in spec/factories/thoughts.rb file

---
+++ spec/factories/thoughts.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: thoughts
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  level      :string
+#  negative   :string
+#  positive   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  student_id :bigint           not null
+#
+# Indexes
+#
+#  index_thoughts_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
+#
+FactoryBot.define do
+  factory :thought do
+    sequence(:email) { |n| "#{n}#{Faker::Internet.unique.email}" }
+    level { "Nível 2" }
+    positive { "Inicio com empolgação na mentoria pelas primeiras vezes" }
+    negative { "Não aproveitar as oportunidades de tirar dúvida na mentoria" }
+    association :student, factory: :student
+  end
+end
