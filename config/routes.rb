dd1620407ec5f895398a9133d3059e26bf172123
gudiao21 <lucianodantas53@gmail.com>
2023-08-01 19:14:25 -0300

    feat(routes): create routes in config/routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
-  devise_for :users
-  root to: "welcome#index"
-  
-  get '/routines', to: 'routines#index', as: 'routines'
-  post '/routines', to: 'routines#create', as: 'routines_create'
-  get  '/routines/new', to: 'routines#new', as: 'new_routine'
-  
-  get '/routines/:id/pre_delete/', to: 'routines#pre_destroy', as: 'pre_destroy_routine'
-  delete '/routines/:id', to: 'routines#destroy', as: 'delete_routine'
-
-  get '/routines/:id', to: 'routines#show', as: 'show_routine'
-  match '/routines/:id', to: 'routines#update', as: 'update_routine', via: [:post, :patch]
-  get '/routines/:id/edit', to: 'routines#edit', as: 'edit_routine'
+  resources :thoughts
 end
