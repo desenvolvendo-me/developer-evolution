2c3af8ac8d4b936318e6ddf02fbcccc350102bcd
gudiao21 <lucianodantas53@gmail.com>
2023-08-01 09:04:52 -0300

    feat(routes): Add 'module: :micro_goals' on the third line of the config/routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :micro_goals do
-    resources :micro_goal_tasks
+    resources :micro_goal_tasks, module: :micro_goals
   end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
