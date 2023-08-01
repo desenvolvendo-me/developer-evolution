ebe6970e6bec112ecacc1f79766a8febb850e4c4
gudiao21 <lucianodantas53@gmail.com>
2023-08-01 20:42:30 -0300

    feat(controller): create app/controllers/thoughts_controller.rb file

---
+++ app/controllers/thoughts_controller.rb
@@ -0,0 +1,47 @@
+class ThoughtsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_thought, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @thoughts = Thought.all
+  end
+
+  def new
+    @thought = Thought.new
+  end
+
+  def create
+    @thought = current_user.student.thoughts.build(thought_params)
+
+    if @thought.save
+      redirect_to action: :show, id: @thought.id, notice: 'thought was successfully created.'
+    else
+      render :new, flash[:notice] = 'thought was not created.'
+    end
+  end
+
+  def show;end
+
+  def update
+    if @thought.update(thought_params)
+      redirect_to action: :show, id: @thought.id, flash[:notice] = 'thought was successfully updated.'
+    else
+      redirect_to action: :edit
+    end
+
+    def destroy
+      @thought.destroy
+      redirect_to thoughts_path, notice: 'thought was successfully destroyed.'
+    end
+
+  private
+
+  def set_thought
+    @thought = current_user.student.thoughts.find(params[:id])
+    # end
+  end
+
+  def thought_params
+    params.require(:thought).permit(:level, :positive, :negative, :student_id)
+  end
+end
