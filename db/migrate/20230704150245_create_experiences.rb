8b4b417d0dccf99ba8bbd78f45f5c38f40ee1a1e
gudiao21 <lucianodantas53@gmail.com>
2023-08-01 13:36:44 -0300

    fix(migration): return back db/migrate/20230704150245_create_experiences.rb

---
+++ db/migrate/20230704150245_create_experiences.rb
@@ -0,0 +1,13 @@
+class CreateExperiences < ActiveRecord::Migration[7.0]
+  def change
+    create_table :experiences do |t|
+      t.references :student, null: false, foreign_key: true
+      t.text :content
+      t.string :category
+      t.integer :level
+      t.integer :week
+
+      t.timestamps
+    end
+  end
+end
