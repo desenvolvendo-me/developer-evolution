11e5b92e1566ccb73bea0ae309a02744c961aa80
gudiao21 <lucianodantas53@gmail.com>
2023-08-02 07:59:27 -0300

    feature(model): Add 'enum' command in app/models/thought.rb

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,6 +1,29 @@
+# == Schema Information
+#
+# Table name: thoughts
+#
+#  id         :bigint           not null, primary key
+#  level      :string
+#  negative   :text
+#  positive   :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  student_id :bigint           not null
+#
+# Indexes
+#
+#  index_thoughts_on_student_id  (student_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (student_id => students.id)
+#
 class Thought < ApplicationRecord
   # Specify the table name explicitly since it's not following the Rails naming convention
   self.table_name = 'thoughts'
 
   belongs_to :student
+
+  LEVEL_OPTIONS = %w[1 2 3].freeze
+  enum level: Hash[LEVEL_OPTIONS.map { |option| [option, option] }]
 end
