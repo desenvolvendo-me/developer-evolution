94303cb691adee1e9f581ef460d805c0f767824a
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-08-03 14:17:55 -0300

    feat(controller): Edit controller Course_basics

--- app/controllers/courses/course_basics_controller.rb
+++ app/controllers/courses/course_basics_controller.rb
@@ -18,14 +18,8 @@ class Courses::CourseBasicsController < ApplicationController
     # @message = Message.new(message_params)
     @course_basic = @course.course_basics.build(course_basic_params)
 
-    respond_to do |format|
-      if @course_basic.save
-        format.html { redirect_to [@course, @course_basic], notice: "Message was successfully created." }
-        format.json { render :show, status: :created, location: @course_basic }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
+    redirect_to(@course_basic.save ? [@course, @course_basic] : { action: "new" },
+                notice: @course_basic.save ? t('controller.flash_create') : t('controller.flash_fail'))
   end
   def update
     redirect_to @course_basic.update(course_basic_params) ? [@course, @course_basic] : edit_course_course_basic_path(@course, @course_basic), notice: @course_basic.update(course_basic_params) ? t('controller.flash_update') : t('controller.flash_fail_purpose')
@@ -44,9 +38,10 @@ class Courses::CourseBasicsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_course_basic
     @course_basic = @course.course_basics.find(params[:id])
+    @number_of_course = @course_basic.number_of_course
   end
 
   def course_basic_params
-    params.require(:course_basic).permit(:link_course, :link_repository, :link_certified, :avaliation_course, :avaliation_apprenticeship)
+    params.require(:course_basic).permit(:link_course, :link_repository, :link_certified, :avaliation_course, :avaliation_apprenticeship, :number_of_course)
   end
 end
