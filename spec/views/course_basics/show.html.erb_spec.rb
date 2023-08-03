fceaaa762e12373d2b892aab81dcdedd67db5b12
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-08-03 17:28:27 -0300

    feat(spec): Removed file

--- spec/views/course_basics/show.html.erb_spec.rb
+++ spec/views/course_basics/show.html.erb_spec.rb
@@ -1,24 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "course_basics/show", type: :view do
-  before(:each) do
-    @course_basic = assign(:course_basic, CourseBasic.create!(
-      link_course: "Link Course",
-      link_repository: "Link Repository",
-      link_certified: "Link Certified",
-      avaliation_course: "Avaliation Course",
-      avaliation_apprenticeship: "Avaliation Apprenticeship",
-      course: nil
-    ))
-  end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Link Course/)
-    expect(rendered).to match(/Link Repository/)
-    expect(rendered).to match(/Link Certified/)
-    expect(rendered).to match(/Avaliation Course/)
-    expect(rendered).to match(/Avaliation Apprenticeship/)
-    expect(rendered).to match(//)
-  end
 end
