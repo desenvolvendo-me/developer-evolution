29e0cabf9c561549d4a5cf7dff80d02905cd3bf5
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-08-03 17:29:18 -0300

    feat(spec/courses): Removed file

--- spec/views/courses/show.html.erb_spec.rb
+++ spec/views/courses/show.html.erb_spec.rb
@@ -1,14 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "courses/show", type: :view do
-  before(:each) do
-    @course = assign(:course, Course.create!(
-      type_course: "Type Course"
-    ))
-  end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Type Course/)
-  end
 end
