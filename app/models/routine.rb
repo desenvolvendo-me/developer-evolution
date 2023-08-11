f08f314b1c7f370f501251a719e50dadd8eba8e7
gudiao21 <lucianodantas53@gmail.com>
2023-08-10 22:28:14 -0300

    refactor(model): remove unnecessary vadidation and method

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -22,13 +22,5 @@
 class Routine < ApplicationRecord
   belongs_to :student
   enum day_of_the_week: [ "domingo",  "segunda", "terça", "quarta", "quinta", "sexta", "sábado" ]
-  validates :activity, :priority, presence: true
 
-  #Estou com dúvida se é realmente necessário essas linhas abaixo:
-  def self.weekly_activities
-    [
-      { day_of_th_week: 1, time: '08:00', activity: 'Study', priority: 'high'},
-      { day_of_the_week: 2, time: '10:00', activity: 'work', priority: 'medium'}
-    ]
-  end
 end
