fe4295dcd523fdbe623b6ddf0f66acbef43775f8
gudiao21 <lucianodantas53@gmail.com>
2023-08-14 21:01:36 -0300

    fix(controller): refactor 'app/controllers/routines_controller.rb', because I changed the 'config/routes.rb' file.

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,19 +1,18 @@
 class RoutinesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_routine, only: [:show, :edit, :pre_destroy]
-  
+  before_action :set_routine, only: [:show]
+
   def index
     if current_user.nil?
       redirect_to new_user_session_path
-    else  
+    else
       @routines = Routine.all.limit(6)
-    end  
+    end
   end
 
-  def show
-    
+  def show;
   end
-  
+
   def new
     @routine = Routine.new
   end
@@ -21,9 +20,9 @@ class RoutinesController < ApplicationController
   def create
     @routine = Routine.new(routine_params)
     @routine.student_id = current_user.student.id
-  
+
     if @routine.save
-      flash[:notice] = "Rotina criada com sucesso."
+      flash[:notice] = t('controller.routine.create.notice')
       redirect_to routines_path(@routine)
     else
       logger.error @routine.errors.full_messages
@@ -31,66 +30,42 @@ class RoutinesController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
-    
+    @routine = Routine.find(params[:id])
   end
 
   def update
-    #debugger
     @routine = Routine.find(params[:id])
     if @routine.update(routine_params)
-        flash[:notice] = "Rotina atualizada com sucesso."
-        redirect_to routines_path(@routine)
+      flash[:notice] = t('controller.routine.update.notice')
+      redirect_to routines_path(@routine)
     else
-        render :edit
-    end  
+      render :edit
+    end
   end
 
-  def pre_destroy
-    if @routine
-      #debugger
-      render 'pre_destroy_routine'
-    else
-      flash[:error] = 'Veículo não encontrado.'
-      render 'index'
-    end  
-  end
-  
   def destroy
-    #debugger
     @routine = Routine.find_by(id: params[:id])
-    
+
     if @routine
       if @routine.destroy
-        flash[:notice] = 'Rotina excluída com sucesso.'
+        flash[:notice] = t('controller.routine.destroy.notice_deleted')
       else
-        flash[:error] = 'Erro ao excluir a Rotina.'
+        flash[:error] = t('controller.routine.destroy.notice_error')
       end
-    else
-      flash[:error] = 'Rotina não encontrada.'
     end
     redirect_to routines_path
   end
 
+
   private
 
   def set_routine
-    if params[:id] == "pre_delete"
-      # Lógica para tratamento especial de pré-exclusão
-      # Por exemplo, você pode definir uma variável de instância @routine com um objeto fictício
-      @routine = Routine.new
-      @routine.id = -1
-      @routine.day_of_the_week = "Segunda-feira"
-      @routine.hour = "10:00"
-      @routine.activity = "Atividade de exemplo"
-      @routine.priority = 1
-    else
-      @routine = Routine.find(params[:id])
-    end
+    @routine = current_user.student.routines.find_by(id: params[:id])
   end
 
   def routine_params
-    params.require(:routine).permit(:day_of_the_week, :hour, :activity, :priority)
+    params.require(:routine).permit(:day_of_the_week, :hour, :activity, :priority, :student_id)
   end
 end
