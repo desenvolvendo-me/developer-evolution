6cf95e0f353ea29f7e3c91435f681bcca258ab01
gudiao21 <lucianodantas53@gmail.com>
2023-08-17 15:07:03 -0300

    refactor(routes): resolve conflict in 'config/routes.rb'

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
-  resources :goals do
-    resources :goal_questions, module: :goals
-  end
-
-  resources :experiences
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   devise_for :users
   root to: "welcome#index"
   get 'welcome/index'
-end
 
+  resources :courses do
+    resources :course_basics, module: :courses
+  end
+
+  resources :objectives do
+    resources :objective_questions
+  end
+
+  resources :goals do
+    resources :goal_questions, module: :goals
+  end
+
+  resources :experiences
+
+end
