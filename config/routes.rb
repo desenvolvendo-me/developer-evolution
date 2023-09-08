3ef6cff0681d691c78fbafda4f0e4270be970ea5
Marco Castro <marcodotcastro@gmail.com>
2023-09-08 16:12:31 -0300

    chore: remove future conflict
    # Conflicts:
    #	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,44 +15,45 @@ Rails.application.routes.draw do
   scope 'level' do
     scope 'zero' do
       resources :routines
+      resources :thoughts
     end
-    resources :thoughts
-    resources :experiences
+  end
 
-    resources :scripts do
-      resources :script_questions
+  resources :experiences
+
+  resources :scripts do
+    resources :script_questions
+  end
+
+  resources :interviews do
+    resources :interview_videos
+  end
+
+  resources :purposes do
+    resources :purpose_questions, module: :purposes
+  end
+
+  resources :courses do
+    resources :course_basics, module: :courses
+  end
+
+  resources :objectives do
+    resources :objective_questions
+  end
+
+  resources :goals do
+    resources :goal_questions, module: :goals
+  end
+
+  resources :tests do
+    resources :tests_battles do
+      resources :tests_issues
     end
+  end
 
-    resources :interviews do
-      resources :interview_videos
-    end
-
-    resources :purposes do
-      resources :purpose_questions, module: :purposes
-    end
-
-    resources :courses do
-      resources :course_basics, module: :courses
-    end
-
-    resources :objectives do
-      resources :objective_questions
-    end
-
-    resources :goals do
-      resources :goal_questions, module: :goals
-    end
-
-    resources :tests do
-      resources :tests_battles do
-        resources :tests_issues
-      end
-    end
-
-    resources :preparations do
-      resources :preparations_releases do
-        resources :preparations_issues
-      end
+  resources :preparations do
+    resources :preparations_releases do
+      resources :preparations_issues
     end
   end
 end
